eb1e627d0cc704662af809168b44f8816287f90a
Harun1804 <Harun.arrasyid1804@gmail.com>
2025-07-25 13:42:43 +0700

    Initial

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	app := gin.Default()
+	app.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+	app.Run(":8080")
+}
