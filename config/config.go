fe2c33266cf48d2790998e5582e9a5e3e0154ae6
Harun1804 <Harun.arrasyid1804@gmail.com>
2025-07-25 13:51:45 +0700

    add env & load env

---
+++ config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+}
+
+func GetEnv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
+}
--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"galaxy/backend-api/config"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	// Load environment variables from .env file
+	config.LoadEnv()
+
 	app := gin.Default()
 	app.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "Hello, World!",
 		})
 	})
-	app.Run(":3000")
+	app.Run(":" + config.GetEnv("APP_PORT", "3000"))
 }
